ec849732184b3aa3012f2e7011122490aa27aa69
Sascha Teske <sascha.teske@gmail.com>
2013-09-09 03:38:41 +0200

    Fix package.json

---
+++ app/controllers/gnugo.js
@@ -0,0 +1,17 @@
+exports.GnuGoCtrlr = {
+  estimate: function (req, res) {
+    res.send(
+      'nothing to see, yet\n' +
+      'except, perhaps:\n' +
+      this
+    );
+  },
+  play:  function (req, res) {
+    res.send(
+      'nothing to see, yet\n' +
+      'except, perhaps:\n' +
+      this
+    );
+  }
+};
+
